e43402541c7c022c4a0ce450ff57ba57d4e797a2
watwatos <alichamas.22@hotmail.com>
2024-03-28 17:58:40 +0200

    updated-sticky-notes-desgin

--- src/pages/sticky/components/NoteCard.jsx
+++ src/pages/sticky/components/NoteCard.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NoteCard = ({ note, urgent, deleteNote, setDraggable }) => {
+  const [dragged, setDragged] = useState(false);
   return (
     <div
-      onDrag={() => setDraggable(note)}
+      onDrag={() => {
+        setDraggable(note);
+      }}
+      onDragStart={() => setDragged(true)}
+      onDragEnd={() => setDragged(false)}
       draggable="true"
       className={`note-card p bg-secondary border-radius ${
         !urgent ? " bg-one" : "bg-two"
-      }`}
+      } ${dragged && "opacity"}`}
     >
       <p> {note.note}</p>
       <small onClick={() => deleteNote(note)}>X</small>
